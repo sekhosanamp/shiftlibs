f2d2dd2cdd5c7e3174b6a95e989ae5d712372c8a
Mmako Sekhosana <mmako@glucode.com>
2022-03-23 00:07:02 +0200

    Working copy

---
+++ ShiftLibrary/ShiftLibraryCalculate.swift
@@ -0,0 +1,22 @@
+//
+//  ShiftLibraryCalculate.swift
+//  ShiftLibrary
+//
+//  Created by Mmako Sekhosana on 2022/03/22.
+//
+
+import Foundation
+
+class ShiftLibraryCalculate {
+
+    let name = "ShiftLibraryCalculate"
+
+    public func addIntegers(a: Int, b: Int) -> Int {
+        return a + b
+    }
+
+    public func substractIntegers(a: Int, b: Int) -> Int {
+        return a - b
+    }
+
+}
